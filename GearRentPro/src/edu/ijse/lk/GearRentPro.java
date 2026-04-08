92557516c01314fb44c18d6ade1607a4cf909c58
Pulindu000 <pulinduvidmal80@gmail.com>
2026-04-08 12:24:41 +0530

    Implemented java swing look and feel to improve the style

--- GearRentPro/src/edu/ijse/lk/GearRentPro.java
+++ GearRentPro/src/edu/ijse/lk/GearRentPro.java
@@ -4,6 +4,10 @@
  */
 package edu.ijse.lk;
 
+import edu.ijse.lk.ui.LoginFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 /**
  *
  * @author Pulindu
@@ -14,6 +18,13 @@ package edu.ijse.lk;
 public class GearRentPro {
     public static void main(String[] args) {
         
+        //Java swing look and feel found from geekforgeek to improve design
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.out.println("Look and Feel not set: " + e.getMessage());
+        }
+        SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
     }
 }
 
